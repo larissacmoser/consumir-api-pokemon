f31487cea70955bc3fc21cb04ace86acb2e17732
AdrianoCoutinho <adriano.98@outlook.com>
2022-12-13 22:27:21 -0300

    quase la

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,115 +1,65 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
+  Divider,
   Grid,
+  IconButton,
+  InputBase,
   Paper,
   TextField,
   Typography,
 } from "@mui/material";
 
-import { ProductType } from "../types";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { useNavigate } from "react-router-dom";
-import Message from "../components/Message";
-import FormProduct from "../components/FormProduct/FormProduct";
-import {
-  productAdd,
-  productDelete,
-  productsGetAll,
-  selectProducts,
-} from "../store/modules/ProductsSlice";
-import { ProductSaveType } from "../types/ProductType";
-import ItemProduct from "../components/ItemProduct/ItemProduct";
+import SearchIcon from "@mui/icons-material/Search";
+
+import { useAppDispatch } from "../store/hooks";
 
 const Products: React.FC = () => {
-  const [openEdit, setOpenEdit] = useState<boolean>(false);
-  const productsRedux = useAppSelector(selectProducts);
+  // const productsRedux = useAppSelector(selectProducts);
+  const [pokemon, setPokemon] = useState<string>("");
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    // dispatch(productsGetAll());
+  }, []);
+
+  const handleClick = () => {
+    console.log("Executou");
     dispatch(productsGetAll());
-  }, []);
-
-  const handleAddProduct = useCallback((product: ProductSaveType) => {
-    dispatch(productAdd(product));
-  }, []);
-
-  const handleDeleteContact = useCallback((product: ProductType) => {
-    console.log("entrou aqiui");
-    dispatch(productDelete(product.id));
-  }, []);
-
-  const openEditModal = useCallback(() => {
-    setOpenEdit(true);
-  }, []);
-
-  const handleEditContact = useCallback((product: ProductType) => {
-    // dispatch(
-    //   updateContact({
-    //     id: contact.phone,
-    //     changes: { name: `${contact.name} Editado` },
-    //   })
-    // );
-  }, []);
-
-  const handleClose = () => {
-    setOpenEdit(false);
   };
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Typography variant="h5">Produtos</Typography>
+        <Typography variant="h5">Pokémons</Typography>
       </Grid>
       <Grid item xs={12}>
-        <FormProduct action={handleAddProduct} />
-        <Message />
-      </Grid>
-      <Grid item xs={12}>
-        <Paper elevation={2} sx={{ padding: "5px" }}>
-          {productsRedux.map((item) => {
-            return (
-              <ItemProduct
-                key={item.id}
-                product={item}
-                actionDelete={() => handleDeleteContact(item)}
-                actionEdit={openEditModal}
-              />
-            );
-          })}
-        </Paper>
-      </Grid>
-      <Dialog open={openEdit} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="outlined"
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Buscar Pokémon"
+            inputProps={{ "aria-label": "Buscar Pokémon" }}
+            value={pokemon}
+            onChange={(ev) => setPokemon(ev.target.value)}
           />
-        </DialogContent>
-        <DialogActions>
-          <Button variant="outlined" onClick={handleClose}>
-            Cancelar
-          </Button>
-          <Button variant="contained" onClick={handleClose}>
-            Salvar
-          </Button>
-        </DialogActions>
-      </Dialog>
+          <IconButton
+            onClick={handleClick}
+            type="button"
+            sx={{ p: "10px" }}
+            aria-label="search"
+          >
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        {pokemon}
+      </Grid>
     </Grid>
   );
 };
